dd74c14d2c623ce35170a636614f3f4f46928f03
syedaElhamS <79485383+syedaElhamS@users.noreply.github.com>
2021-05-23 22:51:29 +0200

    Update longestString.js

--- longestString.js
+++ longestString.js
@@ -1,1 +1,8 @@
 // TODO: Write a function that accepts an array of strings. Return the longest string in the array.
+function largestString(strArr) { // the function largestString will accept an array of strings
+	return strArr.reduce (function (prev, current)                // this return statement returns the longest string in the array. Using reduce function gives o(n) complexity.                                                        
+                 {
+                  return prev.length > current.length ? prev : current;   // if the previous element in the array is greater than current element than 
+                                                                          // prev is returned otherwise current is returned
+                 });
+}
